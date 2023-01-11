a087ab9423c13d960b27d42203747e8dffb4b571
aryandesai <51454730+aryandesai@users.noreply.github.com>
2023-01-11 20:03:10 +0530

    Part 2: Solution
    Part 2: Solution

---
+++ Restaurant_project -Student version/src/test/java/RestaurantServiceTest.java
@@ -0,0 +1,98 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalTime;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class RestaurantServiceTest {
+
+    RestaurantService service = new RestaurantService();
+    Restaurant restaurant;
+    //REFACTOR ALL THE REPEATED LINES OF CODE
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>SEARCHING<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    @Order(1)
+    public void searching_for_existing_restaurant_should_return_expected_restaurant_object() throws restaurantNotFoundException {
+        //WRITE UNIT TEST CASE HERE
+    	Restaurant newRestaurant;
+    	 LocalTime openingTime = LocalTime.parse("10:30:00");
+         LocalTime closingTime = LocalTime.parse("22:00:00");
+         newRestaurant = service.addRestaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+         newRestaurant.addToMenu("Sweet corn soup",119);
+         newRestaurant.addToMenu("Vegetable lasagne", 269);
+         
+         assertEquals(newRestaurant,service.findRestaurantByName("Amelie's cafe"));
+    }
+
+    //You may watch the video by Muthukumaran on how to write exceptions in Course 3: Testing and Version control: Optional content
+    @Test
+    @Order(3)
+    public void searching_for_non_existing_restaurant_should_throw_exception() throws restaurantNotFoundException {
+        //WRITE UNIT TEST CASE HERE
+    	LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant = service.addRestaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+        
+        assertThrows(restaurantNotFoundException.class,()->service.findRestaurantByName("John's cafe"));
+    }
+    //<<<<<<<<<<<<<<<<<<<<SEARCHING>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>ADMIN: ADDING & REMOVING RESTAURANTS<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    @Order(4)
+    public void remove_restaurant_should_reduce_list_of_restaurants_size_by_1() throws restaurantNotFoundException {
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant = service.addRestaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        int initialNumberOfRestaurants = service.getRestaurants().size();
+        service.removeRestaurant("Amelie's cafe");
+        assertEquals(initialNumberOfRestaurants-1, service.getRestaurants().size());
+    }
+
+    @Test
+    @Order(5)
+    public void removing_restaurant_that_does_not_exist_should_throw_exception() throws restaurantNotFoundException {
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant = service.addRestaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        assertThrows(restaurantNotFoundException.class,()->service.removeRestaurant("Pantry d'or"));
+    }
+
+    @Test
+    @Order(2)
+    public void add_restaurant_should_increase_list_of_restaurants_size_by_1(){
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant = service.addRestaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+
+        int initialNumberOfRestaurants = service.getRestaurants().size();
+        service.addRestaurant("Pumpkin Tales","Chennai",LocalTime.parse("12:00:00"),LocalTime.parse("23:00:00"));
+        assertEquals(initialNumberOfRestaurants + 1,service.getRestaurants().size());
+    }
+    //<<<<<<<<<<<<<<<<<<<<ADMIN: ADDING & REMOVING RESTAURANTS>>>>>>>>>>>>>>>>>>>>>>>>>>
+    
+  
+    
+    
+    
+    
+    
+}
